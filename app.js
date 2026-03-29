70b30c7b9caf839adac75ae6463044baaac2c8b7
Priya Gajera <piagajera05@gmail.com>
2026-03-29 17:00:01 +0530

    fix session config

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const session = require("express-session");
 const passport = require("passport");
 const flash = require("connect-flash");
 const Category = require("./models/category");
-var MongoStore = require("connect-mongo")(session);
+const MongoStore = require("connect-mongo");
 const connectDB = require("./config/db");
 
 const app = express();
@@ -17,6 +17,7 @@ require("./config/passport");
 
 // mongodb configuration
 connectDB();
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -30,18 +31,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+// ✅ FIXED SESSION CONFIG
 app.use(
   session({
-    secret: process.env.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET || "myfallbacksecret",
     resave: false,
     saveUninitialized: false,
-    store: new MongoStore({
-      mongooseConnection: mongoose.connection,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
     }),
-    //session expires after 3 hours
     cookie: { maxAge: 60 * 1000 * 60 * 3 },
   })
 );
+
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
@@ -62,12 +65,12 @@ app.use(async (req, res, next) => {
 });
 
 // add breadcrumbs
-get_breadcrumbs = function (url) {
+const get_breadcrumbs = function (url) {
   var rtn = [{ name: "Home", url: "/" }],
-    acc = "", // accumulative url
+    acc = "",
     arr = url.substring(1).split("/");
 
-  for (i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length; i++) {
     acc = i != arr.length - 1 ? acc + "/" + arr[i] : null;
     rtn[i + 1] = {
       name: arr[i].charAt(0).toUpperCase() + arr[i].slice(1),
@@ -76,39 +79,40 @@ get_breadcrumbs = function (url) {
   }
   return rtn;
 };
+
 app.use(function (req, res, next) {
   req.breadcrumbs = get_breadcrumbs(req.originalUrl);
   next();
 });
 
-//routes config
+// routes config
 const indexRouter = require("./routes/index");
 const productsRouter = require("./routes/products");
 const usersRouter = require("./routes/user");
 const pagesRouter = require("./routes/pages");
+
 app.use("/products", productsRouter);
 app.use("/user", usersRouter);
 app.use("/pages", pagesRouter);
 app.use("/", indexRouter);
 
-// catch 404 and forward to error handler
+// catch 404
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
 
 var port = process.env.PORT || 3001;
 app.set("port", port);
+
 app.listen(port, () => {
   console.log("Server running at port " + port);
 });
